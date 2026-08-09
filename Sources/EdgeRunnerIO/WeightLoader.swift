749ed263ff169694a607e00ca359204f0d007364
sergejstepanov24102006-png <sergejstepanov24102006@gmail.com>
2026-08-09 18:51:56 +0300

    Delete Sources/EdgeRunnerIO directory

--- Sources/EdgeRunnerIO/WeightLoader.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Metal
-
-public protocol EdgeRunnerWeightLoader: Sendable {
-    var modelConfig: ModelConfig { get }
-
-    func canLoad(url: URL) -> Bool
-    func load(from url: URL) async throws -> WeightMap
-}
